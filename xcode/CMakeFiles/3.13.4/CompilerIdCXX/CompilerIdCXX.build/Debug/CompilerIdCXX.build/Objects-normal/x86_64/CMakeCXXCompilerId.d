e1a6c818626416b49a174a5d481f27360a8595c3
Seong-Hwan Jun <s2jun.uw@gmail.com>
2019-10-01 15:11:48 +0200

    add xcode project files

---
+++ xcode/CMakeFiles/3.13.4/CompilerIdCXX/CompilerIdCXX.build/Debug/CompilerIdCXX.build/Objects-normal/x86_64/CMakeCXXCompilerId.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/seonghwanjun/Dropbox/Research/single-cell-research/repos/tumor_coal/xcode/CMakeFiles/3.13.4/CompilerIdCXX/CMakeCXXCompilerId.cpp
---
+++ xcode/tumor_coal.build/Debug/ALL_BUILD.build/Script-9645B9DE3CD74D00834C4AD6.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+make -C /Users/seonghwanjun/Dropbox/Research/single-cell-research/repos/tumor_coal/xcode -f /Users/seonghwanjun/Dropbox/Research/single-cell-research/repos/tumor_coal/xcode/CMakeScripts/ALL_BUILD_cmakeRulesBuildPhase.make$CONFIGURATION all

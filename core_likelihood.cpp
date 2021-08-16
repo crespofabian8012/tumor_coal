cd3d879051260554b108b7778a21c457b84ff36a
Fausto Fabian Crespo Fernandez <faustofabiancrespofernandez@MacBook-Pro-de-Fausto.local>
2021-08-17 00:14:21 +0200

    files now inside inference

--- core_likelihood.cpp
+++
@@ -1,8 +0,0 @@
-//
-//  core_likelihood.cpp
-//  run
-//
-//  Created by Fausto Fabian Crespo Fernandez on 11/08/2021.
-//
-
-#include "core_likelihood.hpp"
--- genotype_models.cpp
+++
@@ -1,8 +0,0 @@
-//
-//  genotype_models.cpp
-//  run
-//
-//  Created by Fausto Fabian Crespo Fernandez on 09/08/2021.
-//
-
-#include "genotype_models.hpp"

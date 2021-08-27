1c2ad58bcf941c563a05c91194c23f474beb6ac8
Fausto Fabian Crespo Fernandez <faustofabiancrespofernandez@MacBook-Pro-de-Fausto.local>
2021-08-27 17:09:46 +0200

    moved files into inference folder

--- genotype_models.cpp
+++
@@ -1,8 +0,0 @@
-//
-//  genotype_models.cpp
-//  tumor_coal
-//
-//  Created by Fausto Fabian Crespo Fernandez on 21/08/2021.
-//
-
-#include <stdio.h>
--- tree.cpp
+++
@@ -1,8 +0,0 @@
-//
-//  tree.cpp
-//  run
-//
-//  Created by Fausto Fabian Crespo Fernandez on 26/08/2021.
-//
-
-#include "tree.hpp"
